a3baf1fefead9d12d1b507082a4a05faa7b795c1
jtang93 <tang.jonathan93@gmail.com>
2018-09-14 11:19:37 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,6 +19,8 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         value.delete_if("strawberry")
       end
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
